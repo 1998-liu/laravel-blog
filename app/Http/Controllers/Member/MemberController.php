3b12804fe0c6d2d108ef255ab0fd6cdf580a32f2
1998-liu <2376038291@qq.com>
2021-01-15 16:10:16 +0800

    控制器返回视图demo和添加可忽视上传的文件夹

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -8,7 +8,11 @@ class MemberController extends Controller
 {
     public function info($id)
     {
-        return 'memberInfo-id-' . $id;
+//        return 'memberInfo-id-' . $id;
 //        return route('memberInfo');
+        return view('member/info', [
+            'name' => 'James',
+            'age'  => 19,
+        ]);
     }
 }
